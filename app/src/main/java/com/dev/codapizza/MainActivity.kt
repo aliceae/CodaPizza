53a27eb0a2401b8e88a34293104b09bfec9ffe73
alice <aliceae.h@gmail.com>
2023-02-17 15:30:44 +0800

    [ch26]Jetpack Compose

--- app/src/main/java/com/dev/codapizza/MainActivity.kt
+++ app/src/main/java/com/dev/codapizza/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.dev.codapizza
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.dev.codapizza.ui.PizzaBuilderScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            PizzaBuilderScreen()
+        }
     }
 }
